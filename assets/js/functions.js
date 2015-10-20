841dd3fd6feb34de4662b33d47f83453f634b08d
Tsvetelina Stoykova <cveti.stoikova@gmail.com>
2015-10-20 21:46:47 +0300

    updated html/css

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -70,6 +70,40 @@ function workLoad() {
 }
 
 
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
+
+
 
 
 
@@ -78,4 +112,7 @@ $(function() {
 	smoothScroll(1000);
 	workBelt();
 	workLoad();
+
+  $("header h1").fitText(1.2, { minFontSize: '20px', maxFontSize: '72px' });
+  
 });
